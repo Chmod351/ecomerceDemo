db7d184c750a9098aac03f9b2684accc9a4b571f
yamil351 <leokasper@live.com.ar>
2023-06-09 10:28:50 -0300

    pagination fixed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,6 +7,9 @@ import { BASE_URL } from '../requestMethods';
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.bg};
+`;
+
+const Wrapper = styled.div`
   padding: 1.25rem;
   display: flex;
   flex-wrap: wrap;
@@ -14,13 +17,41 @@ const Container = styled.div`
   ${mobile({ alignItems: 'center', justifyContent: 'center' })}
 `;
 
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+`;
+
+const PageButton = styled.button`
+  background-color: ${(props) => (props.active ? '#333' : '#fff')};
+  color: ${(props) => (props.active ? '#fff' : '#333')};
+  border: 1px solid #ccc;
+  padding: 8px 16px;
+  margin: 0 4px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: ${(props) => (props.active ? 'bold' : 'normal')};
+
+  &:hover {
+    background-color:  ${({ theme }) => theme.hover};;
+    color:${({ theme }) => theme.soft};
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
 const Products = ({ tag, filters, sort }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(8);
+  const [totalPages, setTotalPages] = useState(0);
 
-  useEffect(() => {
+  const productAndPages = useEffect(() => {
     const getProducts = async () => {
       try {
         const res = await axios.get(
@@ -31,11 +62,11 @@ const Products = ({ tag, filters, sort }) => {
         setProducts(res.data.products);
         setTotalPages(res.data.totalPages);
       } catch (err) {
-        console.log(error)
+        console.log(err);
       }
     };
     getProducts();
-  }, [tag]);
+  }, [tag, currentPage, pageSize]);
 
   useEffect(() => {
     tag &&
@@ -64,23 +95,57 @@ const Products = ({ tag, filters, sort }) => {
     }
   }, [sort]);
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    productAndPages;
+  };
+
   return (
     <Container>
-      {tag
-        ? filteredProducts.map((item) => (
-            <Product
-              item={item}
-              key={item._id}
-              price={item.price}
-            />
-          ))
-        : products.map((item) => (
-            <Product
-              item={item}
-              key={item._id}
-              price={item.price}
-            />
+      
+      <Wrapper>
+        {tag
+          ? filteredProducts.map((item) => (
+              <Product
+                item={item}
+                key={item._id}
+                price={item.price}
+              />
+            ))
+          : products.map((item) => (
+              <Product
+                item={item}
+                key={item._id}
+                price={item.price}
+              />
+            ))}
+      </Wrapper>
+      {/* Renderizar paginación */}
+      {totalPages > 1 && (
+        <PaginationContainer>
+          <PageButton
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </PageButton>
+          {[...Array(totalPages)].map((_, index) => (
+            <PageButton
+              key={index + 1}
+              active={index + 1 === currentPage}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </PageButton>
           ))}
+          <PageButton
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </PageButton>
+        </PaginationContainer>
+      )}
     </Container>
   );
 };
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000/api/';
+export const BASE_URL = 'http://localhost:5000/api';
 
 const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
 const currentUser = user && JSON.parse(user).currentUser;

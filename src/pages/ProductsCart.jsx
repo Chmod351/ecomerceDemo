35079661e3f7b475935a78567b4c4b52ca60bcaa
yamil351 <leokasper@live.com.ar>
2023-07-01 19:33:33 -0300

    productsCart refactored

---
+++ src/pages/ProductsCart.jsx
@@ -0,0 +1,183 @@
+import { addProduct, removeProduct } from '../redux/cartRedux';
+import { handleSuccess } from '../utils/toast';
+import Summary from '../components/Summary';
+import QuantityButton from '../components/quantityButtons';
+import { SentimentDissatisfiedOutlined } from '@material-ui/icons';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { mobile, pc } from '../responsive';
+import { Link } from 'react-router-dom';
+
+const Container = styled.section``;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: 'column', overflow: 'hidden' })}
+`;
+
+const Info = styled.section`
+  flex: 3;
+  background-color: ${({ theme }) => theme.bg};
+  padding: 0.5rem;
+`;
+
+const Product = styled.article`
+  display: flex;
+
+  justify-content: space-between;
+  ${mobile({ flexDirection: 'column', justifyContent: 'center' })}
+  ${pc({ padding: '0.2rem 0rem' })}
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+  ${mobile({ flexDirection: 'column' })}
+`;
+
+const Image = styled.img`
+  width: 200px;
+  ${mobile({ width: '100vw', padding: '0rem' })}
+`;
+
+const Details = styled.div`
+  color: ${({ theme }) => theme.textSoft};
+  padding: 1.25rem;
+  ${mobile({ flexWrap: 'wrap', width: '100vw' })}
+`;
+
+const ProductName = styled.span``;
+
+const ProductColor = styled.div`
+  width: 1.25rem;
+  height: 1.25rem;
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.textSoft};
+
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 1.875rem;
+  font-weight: 200;
+  ${mobile({ marginBottom: '1.25rem' })}
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const Message = styled.p`
+  display: flex;
+  height: 50vh;
+  flex: 4;
+  background-color: ${({ theme }) => theme.bgLighter};
+  font-weight: bold;
+  font-size: 2.4rem;
+  color: ${({ theme }) => theme.soft};
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  ${mobile({ height: '60vh', flexDirection: 'column' })}
+`;
+
+const IconFace = styled.svg`
+  color: ${({ theme }) => theme.soft};
+  margin: 0 1rem;
+  ${mobile({ margin: '1rem auto' })}
+`;
+
+const ProductsCarts = ({ cart, username }) => {
+  const dispatch = useDispatch();
+  const handleRemove = (index) => {
+    dispatch(removeProduct(cart.products[index]));
+    handleSuccess('removed');
+  };
+
+  const handleAdd = (index) => {
+    dispatch(addProduct({ ...cart.products[index], quantity: 1 }));
+    handleSuccess('added');
+  };
+
+  return (
+    <Container>
+      {cart.products.length > 0 ? (
+        <Bottom role="list">
+          <Info role="complementary">
+            {cart.products.map((product, index) => (
+              <Product tabIndex="0">
+                <ProductDetail>
+                  <Link
+                    to={`/product/${product._id}`}
+                    role="link"
+                    aria-label={`${product.name} in ${
+                      product.color
+                    } total units ${product.quantity} cost = ${
+                      product.price * product.quantity
+                    }`}
+                    title={`${product.name} in ${product.color} total units ${
+                      product.quantity
+                    } cost = ${product.price * product.quantity}`}
+                  >
+                    <Image src={product.imgUrl} alt={product.name} />
+                  </Link>
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> {product.name}
+                    </ProductName>
+                    <ProductColor
+                      color={product.color}
+                      title={product.color}
+                      aria-label={product.color}
+                    />
+                    <ProductSize>
+                      <b>Size:</b> {product.size}
+                    </ProductSize>
+                  </Details>
+                </ProductDetail>
+                <PriceDetail>
+                  {/* add and remove buttons */}
+                  <QuantityButton
+                    add={() => handleAdd(index)}
+                    remove={() => handleRemove(index)}
+                    quantity={product.quantity}
+                  />
+                  <ProductPrice
+                    aria-label="total"
+                    title={product.price * product.quantity}
+                  >
+                    $ {product.price * product.quantity}
+                  </ProductPrice>
+                </PriceDetail>
+              </Product>
+            ))}
+            <Hr />
+          </Info>
+          {/* SUMARY COMPONENT */}
+          <Summary cart={cart} username={username} />
+        </Bottom>
+      ) : (
+        <Message>
+          Your Cart Is Empty
+          <IconFace>
+            <SentimentDissatisfiedOutlined />
+          </IconFace>
+        </Message>
+      )}
+    </Container>
+  );
+};
+export default ProductsCarts;

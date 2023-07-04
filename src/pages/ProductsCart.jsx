5c0a9abff1883c0ac18dbb98b937823770e1e0b1
yamil351 <leokasper@live.com.ar>
2023-07-04 11:25:21 -0300

    conditional to pagination cart

--- src/pages/ProductsCart.jsx
+++ src/pages/ProductsCart.jsx
@@ -106,105 +106,113 @@ const IconFace = styled.svg`
 const ProductsCarts = ({ cart, username }) => {
   const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(cart.products.length/10);
+  const totalPages = Math.ceil(cart.products.length / 10);
 
   const handleRemove = (index) => {
-    dispatch(removeProduct(cart.products[startIndex+index]));
+    dispatch(removeProduct(cart.products[startIndex + index]));
     handleSuccess('removed');
   };
 
   const handleAdd = (index) => {
-    dispatch(addProduct({ ...cart.products[startIndex+index], quantity: 1 }));
+    dispatch(addProduct({ ...cart.products[startIndex + index], quantity: 1 }));
     handleSuccess('added');
   };
 
-     // get start and end index to calculate current products on page
-     let startIndex = (currentPage - 1) * 10;
-     let endIndex = startIndex + 10;
-     let currentProds = cart.products.length < 10 ? cart.products : cart.products.slice(startIndex, endIndex);
+  // get start and end index to calculate current products on page
+  let startIndex = (currentPage - 1) * 10;
+  let endIndex = startIndex + 10;
+  let currentProds =
+    cart.products.length < 10
+      ? cart.products
+      : cart.products.slice(startIndex, endIndex);
 
-     useEffect(()=>{
-      startIndex = (currentPage - 1) * 10;
-      endIndex = startIndex + 10;
-     currentProds = cart.products.length < 10 ? cart.products : cart.products.slice(startIndex, endIndex);
-    },[cart.products.length])
-     
-     const handlePageChange = (page) => {
-      setCurrentPage(page);
-    };
+  useEffect(() => {
+    startIndex = (currentPage - 1) * 10;
+    endIndex = startIndex + 10;
+    currentProds =
+      cart.products.length < 10
+        ? cart.products
+        : cart.products.slice(startIndex, endIndex);
+  }, [cart.products.length]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <Container>
-    {/* if cart.products has no more than 0 items, message and icon will show up */}
-      {cart.products.length > 0 ?
-      (<>
-      <Wrapper role="list">
-          <Info role="complementary">
-            {currentProds.map((product, index) => (
-              <Product tabIndex="0">
-                <ProductDetail>
-                  <Link
-                    to={`/product/${product._id}`}
-                    role="link"
-                    aria-label={`${product.name} in ${
-                      product.color
-                    } total units ${product.quantity} cost = ${
-                      product.price * product.quantity
-                    }`}
-                    title={`${product.name} in ${product.color} total units ${
-                      product.quantity
-                    } cost = ${product.price * product.quantity}`}
-                  >
-                    <Image src={product.imgUrl} alt={product.name} />
-                  </Link>
-                  <Details>
-                    <ProductName>
-                       <b>Product:</b> {product.name}
-                    </ProductName>
-                    <ProductColor
-                      color={product.color}
-                      title={product.color}
-                      aria-label={product.color}
-                    />
-                    <ProductSize>
-                      <b>Size:</b> {product.size}
-                    </ProductSize>
-                    <br></br>
-                    <ProductDescription
-                      title={product.description}
-                      aria-label={product.description}
+      {/* if cart.products has no more than 0 items, message and icon will show up */}
+      {cart.products.length > 0 ? (
+        <>
+          <Wrapper role="list">
+            <Info role="complementary">
+              {currentProds.map((product, index) => (
+                <Product tabIndex="0">
+                  <ProductDetail>
+                    <Link
+                      to={`/product/${product._id}`}
+                      role="link"
+                      aria-label={`${product.name} in ${
+                        product.color
+                      } total units ${product.quantity} cost = ${
+                        product.price * product.quantity
+                      }`}
+                      title={`${product.name} in ${product.color} total units ${
+                        product.quantity
+                      } cost = ${product.price * product.quantity}`}
                     >
-                      <b>Description:</b> {product.description}
-                    </ProductDescription>
-                  </Details>
-                </ProductDetail>
-                <PriceDetail>
-                  {/* add and remove buttons */}
-                  <QuantityButton
-                    add={() => handleAdd(index)}
-                    remove={() => handleRemove(index)}
-                    quantity={product.quantity}
-                  />
-                  <ProductPrice
-                    aria-label="total"
-                    title={product.price * product.quantity}
-                  >
-                    $ {product.price * product.quantity}
-                  </ProductPrice>
-                </PriceDetail>
-              </Product>
-            ))}
-            <Hr />
-          </Info>
-          {/* SUMARY COMPONENT */}
-          <Summary cart={cart} username={username} />
-        </Wrapper>       
-        <Pagination 
-        filteredProducts={currentProds} 
-        totalPages={totalPages} 
-        currentPage={currentPage} 
-        handlePageChange={handlePageChange}
-        />
+                      <Image src={product.imgUrl} alt={product.name} />
+                    </Link>
+                    <Details>
+                      <ProductName>
+                        <b>Product:</b> {product.name}
+                      </ProductName>
+                      <ProductColor
+                        color={product.color}
+                        title={product.color}
+                        aria-label={product.color}
+                      />
+                      <ProductSize>
+                        <b>Size:</b> {product.size}
+                      </ProductSize>
+                      <br></br>
+                      <ProductDescription
+                        title={product.description}
+                        aria-label={product.description}
+                      >
+                        <b>Description:</b> {product.description}
+                      </ProductDescription>
+                    </Details>
+                  </ProductDetail>
+                  <PriceDetail>
+                    {/* add and remove buttons */}
+                    <QuantityButton
+                      add={() => handleAdd(index)}
+                      remove={() => handleRemove(index)}
+                      quantity={product.quantity}
+                    />
+                    <ProductPrice
+                      aria-label="total"
+                      title={product.price * product.quantity}
+                    >
+                      $ {product.price * product.quantity}
+                    </ProductPrice>
+                  </PriceDetail>
+                </Product>
+              ))}
+              <Hr />
+            </Info>
+            {/* SUMARY COMPONENT */}
+            <Summary cart={cart} username={username} />
+          </Wrapper>
+          {cart.products.length > 8 ? (
+            <Pagination
+              filteredProducts={currentProds}
+              totalPages={totalPages}
+              currentPage={currentPage}
+              handlePageChange={handlePageChange}
+            />
+          ) : null}
         </>
       ) : (
         <Message>

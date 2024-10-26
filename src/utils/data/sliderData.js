887d531094b439e19c524c3dc287cc5823822d55
yamiltauil <ytauil@itcrowdarg.com>
2024-10-26 13:37:05 -0300

    replace images

--- src/utils/data/sliderData.js
+++ src/utils/data/sliderData.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 10,
-    img: 'https://images.vexels.com/media/users/3/132735/isolated/preview/7a6a59aba9f4fa37ddd754484bb0475a-woman-fitness-silhouette-by-vexels.png',
+    img: 'https://ae01.alicdn.com/kf/HTB1vdV2oILJ8KJjy0Fnq6AFDpXaK/RLMABABY-White-Button-Women-Tee-Shirt-Sexy-Deep-V-Neck-Long-Sleeve-Crop-Top-Fashion-Spring.jpg',
     title: '30% OFF',
     desc: 'Upgrade your wardrobe with our exclusive limited-time offer! Enjoy a 30% discount on our stylish and versatile t-shirts. Elevate your everyday look with premium quality and unbeatable prices.',
     alt: 'women shirts ',
@@ -9,7 +9,7 @@ export const sliderItems = [
   },
   {
     id: 11,
-    img: 'https://images.vexels.com/media/users/3/135923/isolated/preview/095a0bdb457de09ea1cd2d0bff3f8013-mujer-de-negocios-femenina.png',
+    img: 'https://www.urbasm.com/wp-content/uploads/2020/07/trench-coat-lingerie-sexy-blonde.jpg',
     title: '25% OFF',
     desc: "Discover the perfect outerwear to keep you warm and stylish this season. Don't miss out on this limited-time deal to upgrade your wardrobe with trendy coats that combine fashion and functionality",
     alt: 'women coat',
@@ -17,7 +17,7 @@ export const sliderItems = [
   },
   {
     id: 12,
-    img: 'https://images.vexels.com/media/users/3/131571/isolated/lists/b7a4c3dc8326fd08f360be526a732da2-silueta-femenina-desnuda.png',
+    img: 'https://www.dhresource.com/0x0s/f2-albu-g5-M00-7E-B5-rBVaJFlLNByAIF_EAADD0i_prOE109.jpg/sexy-leather-jacket-women-faux-leather-jackets.jpg',
     title: '20% OFF',
     desc: "Take advantage of this special discount and upgrade your wardrobe with fashion-forward pieces known for their impeccable quality and design. Don't wait any longer, refresh your look with our trendy jackets!",
     alt: 'women jacket',

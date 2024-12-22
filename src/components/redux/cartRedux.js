95b8f474fe8d3d73f2eb73dd55b112df44a7ca5d
yamiltauil <ytauil@itcrowdarg.com>
2024-12-22 15:39:17 -0300

    adapta a la nueva api

--- src/components/redux/cartRedux.js
+++ src/components/redux/cartRedux.js
@@ -17,12 +17,12 @@ const cartSlice = createSlice({
         // El producto ya existe en el carrito
         state.products[existingProductIndex].quantity +=
           action.payload.quantity;
-        state.total += action.payload.price * action.payload.quantity;
+        state.total += action.payload.price_es * action.payload.quantity;
       } else {
         // El producto no existe en el carrito
         state.quantity += 1;
         state.products.push(action.payload);
-        state.total += action.payload.price * action.payload.quantity;
+        state.total += action.payload.price_es * action.payload.quantity;
       }
     },
     removeProduct: (state, action) => {
@@ -37,12 +37,12 @@ const cartSlice = createSlice({
         if (existingProduct.quantity > 1) {
           // Hay más de una instancia del producto en el carrito
           existingProduct.quantity -= 1;
-          state.total -= existingProduct.price;
+          state.total -= existingProduct.price_es;
         } else {
           // Solo hay una instancia del producto en el carrito
           state.quantity -= 1;
           state.products.splice(existingProductIndex, 1);
-          state.total -= existingProduct.price;
+          state.total -= existingProduct.price_es;
         }
       }
     },

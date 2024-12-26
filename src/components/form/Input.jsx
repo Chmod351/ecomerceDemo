14993abaa5fdeca149979758e1a1894db828312c
yamiltauil <ytauil@itcrowdarg.com>
2024-12-26 19:48:53 -0300

    fix styles

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,36 +1,54 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {setUserData} from "../redux/orderRedux";
+import styled from 'styled-components';
+const Column=styled.div`
+  display: flex;
+flex-direction: column;
+`
+
+const Input = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 8px;
+  font-size: 16px;
+  width: 90%;
+`;
 
 function InputField({
   label,
   name,
-  register,
   errors,
   placeholder,
   type = "text",
-  disabled,
-  required,
-  defaultValue,
 }) {
   let error;
-  if (errors.isArray) {
-    error = errors?.[0]?.[name.split(".").join("?.")];
-    console.log("errrrrrrrrrrrr", error);
-  } else {
-    error = errors?.[name.split(".").join("?.")];
-  }
+{/*   if (errors.isArray) { */}
+  {/*   error = errors?.[0]?.[name.split(".").join("?.")]; */}
+  {/*   console.log("errrrrrrrrrrrr", error); */}
+  {/* } else { */}
+  {/*   error = errors?.[name.split(".").join("?.")]; */}
+  {/* } */}
+{/*  */}
+  const dispatch = useDispatch();
+ const handleChange = (field,event) => {
+    dispatch(setUserData({ [field]: event.target.value }));
+  };
 
+
+  
+const userData = useSelector((state) => state.orders);
+console.log(userData)
   return (
-    <div className="flex flex-col w-full ">
+    <Column className="flex flex-col w-full ">
       <label className="font-helvetica text-sm font-bold">{label}</label>
-      <input
+      <Input
         className={`rounded p-4 mt-1 placeholder:text-gray-400  outline-none ${
           error ? "border-red-500" : "border-gray-300"
         }`}
-        {...register(name, { required })}
+        onChange={(event) =>handleChange(name,event)}
         placeholder={placeholder}
         type={type ? type : "text"}
-        disabled={disabled}
-        defaultValue={defaultValue}
       />
       {error && (
         <span className="text-red-500 text-sm mt-1">{error.message}</span>
@@ -40,7 +58,7 @@ function InputField({
           la informacion del stock debe estar completa
         </span>
       )}
-    </div>
+    </Column>
   );
 }
 

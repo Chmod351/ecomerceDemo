060d126c0b60d8138eac2edc331d318f11c13fab
yamiltauil <ytauil@itcrowdarg.com>
2024-12-27 23:17:07 -0300

    fix error and delete route

--- src/utils/data/navbarData.js
+++ src/utils/data/navbarData.js
@@ -5,24 +5,41 @@ export const e = [
 		name: 'Home',
 	},
 	{
-		id: 21,
+		id: 22222,
 		route: '/products/tops',
 		name: 'Tops',
 	},
 	{
-		id: 23,
+		id: 3,
 		route: '/products/bottoms',
 		name: 'Bottoms',
 	},
 
 	{
-		id: 25,
+		id: 4,
 		route: '/products/shoes',
 		name: 'Shoes',
 	},
 	{
-		id: 29,
+		id: 5,
 		route: '/products/accesories',
 		name: 'Accesories',
+	},
+];
+export const adminRoutes = [
+	{
+		id: 6,
+		route: '/admin/dashboard',
+		name: 'Dashboard',
+	},
+	{
+		id: 7,
+		route: '/admin/newproduct',
+		name: 'New Product',
+	},
+	{
+		id: 8,
+		route: '/admin/products',
+		name: 'Products',
 	},
 ];

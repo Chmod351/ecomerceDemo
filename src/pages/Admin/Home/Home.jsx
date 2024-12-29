cde4eaa8a6de75e7eae7ad7ab4ff52a1e133b4f1
yamiltauil <ytauil@itcrowdarg.com>
2024-12-29 16:25:36 -0300

    add dashboard data

--- src/pages/Admin/Home/Home.jsx
+++ src/pages/Admin/Home/Home.jsx
@@ -4,52 +4,95 @@ import FeaturedInfo from '../../../components/admin/featuredInfo/FeaturedInfo';
 import { publicRequest } from '../../../requestMethods';
 import './home.css';
 import { useEffect, useMemo, useState } from 'react';
+import {
+	LineChart,
+	Line,
+	XAxis,
+	CartesianGrid,
+	Tooltip,
+	ResponsiveContainer,
+} from 'recharts';
 
 export default function HomePage() {
-	const [userStats, setUserStats] = useState([]);
+	const [chartData, setChartData] = useState('');
+	const [monthlySales, setMonthlySales] = useState([]);
+	let pendingOrders = 0;
+	let deliveredOrders = 0;
 
-	const MONTHS = useMemo(
-		() => [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
-			'May',
-			'Jun',
-			'Jul',
-			'Agu',
-			'Sep',
-			'Oct',
-			'Nov',
-			'Dec',
-		],
-		[]
-	);
+	const getMonthlySales = async () => {
+		try {
+			const res = await publicRequest.get('/orders/monthly-sales');
+			setMonthlySales(res.data);
+		} catch (e) {
+			/* handleerror */
+			console.log(e);
+		}
+	};
 
 	useEffect(() => {
-		const getStats = async () => {
+		const fetchStockData = async () => {
 			try {
-				const res = await publicRequest.get('/users/stats');
-				res.data.map((item) =>
-					setUserStats((prev) => [
-						...prev,
-						{ name: MONTHS[item._id - 1], 'Active User': item.total },
-					])
-				);
-			} catch {}
+				const res = await publicRequest.get('/products/stock'); // Llama al endpoint de tu API
+				const transformedData = res.data.map((product) => {
+					const totalStock = product.stock.reduce(
+						(sum, stockItem) => sum + stockItem.quantity,
+						0
+					);
+					return { name: product.name, totalStock }; // Transforma el formato
+				});
+				setChartData(transformedData); // Actualiza el estado con los datos transformados
+			} catch (err) {
+				console.error('Error fetching stock data:', err);
+			}
 		};
-		getStats();
-	}, [MONTHS]);
 
+		fetchStockData();
+		getMonthlySales();
+	}, []);
+
+	// Función genérica para calcular cantidad de órdenes y income según estado
+	const calculateOrdersAndIncome = (orders, status) => {
+		let totalOrders = 0;
+		let totalIncome = 0;
+		console.log(orders);
+		orders.forEach((order) => {
+			order.orderItems.forEach((item) => {
+				if (item.status === status) {
+					totalOrders += 1;
+					totalIncome += item.productPrice * item.quantity;
+				}
+			});
+		});
+
+		return { totalOrders, totalIncome };
+	};
+	console;
 	return (
 		<div className="home">
-			<FeaturedInfo />
-			<Chart
-				data={userStats}
-				title="User Analytics"
-				grid
-				dataKey="Active User"
+			<FeaturedInfo
+				pendingOrders={
+					monthlySales.length > 0
+						? calculateOrdersAndIncome(monthlySales, 'Pending')
+						: 0
+				}
+				deliveredOrders={
+					monthlySales.length > 0
+						? calculateOrdersAndIncome(monthlySales, 'Delivered')
+						: 0
+				}
 			/>
+
+			<div className="chart">
+				<h3 className="chartTitle">Stock</h3>
+				<ResponsiveContainer width="100%" aspect={4 / 1}>
+					<LineChart data={chartData}>
+						<XAxis dataKey="name" stroke="#5550bd" />
+						<Line type="monotone" dataKey="totalStock" stroke="#5550bd" />
+						<Tooltip />
+						<CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
 			<div className="homeWidgets">
 				{/*  <WidgetSm /> */}
 				<WidgetLg />

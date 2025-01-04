5fc0f9351a4cb7bf2d3768ac68e992c18c5dda6d
yamiltauil <ytauil@itcrowdarg.com>
2025-01-03 21:46:22 -0300

    delete extra info from date

--- src/utils/logic/orders.js
+++ src/utils/logic/orders.js
@@ -112,9 +112,9 @@ export const formatCreatedAt = (createdAt) => {
 		day: '2-digit',
 		month: '2-digit',
 		year: 'numeric',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
+	 /*  hour: '2-digit', */
+		/* minute: '2-digit', */
+		/* second: '2-digit', */
 	});
 	return formattedDate;
 };

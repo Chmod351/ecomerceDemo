8d666427db3b25b3fd2dbe0e3bdee640829667ed
yamiltauil <ytauil@itcrowdarg.com>
2025-01-03 22:56:03 -0300

    update color schema

--- src/components/ui/SadFaceMsg.jsx
+++ src/components/ui/SadFaceMsg.jsx
@@ -3,33 +3,33 @@ import { mobile } from '../../responsive';
 import { SentimentDissatisfiedOutlined } from '@material-ui/icons';
 
 const Message = styled.p`
-  display: flex;
-  height: 50vh;
-  flex: 4;
-  background-color: ${({ theme }) => theme.bg};
-  font-weight: bold;
-  font-size: 2.4rem;
-  color: ${({ theme }) => theme.soft};
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  ${mobile({ height: '60vh', flexDirection: 'column' })}
+	display: flex;
+	height: 50vh;
+	flex: 4;
+	background-color: ${({ theme }) => theme.bgLighter};
+	font-weight: bold;
+	font-size: 2.4rem;
+	color: ${({ theme }) => theme.soft};
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	${mobile({ height: '60vh', flexDirection: 'column' })}
 `;
 
 const IconFace = styled.svg`
-  color: ${({ theme }) => theme.soft};
-  margin: 0 1rem;
-  ${mobile({ margin: '1rem auto' })}
+	color: ${({ theme }) => theme.soft};
+	margin: 0 1rem;
+	${mobile({ margin: '1rem auto' })}
 `;
 
 const SadFaceMsg = ({ text }) => {
-  return (
-    <Message>
-      {text}
-      <IconFace>
-        <SentimentDissatisfiedOutlined />
-      </IconFace>
-    </Message>
-  );
+	return (
+		<Message>
+			{text}
+			<IconFace>
+				<SentimentDissatisfiedOutlined />
+			</IconFace>
+		</Message>
+	);
 };
 export default SadFaceMsg;

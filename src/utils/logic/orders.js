56b1c01c6c0d8f9bfa70907912234bdd5ca90700
yamiltauil <ytauil@itcrowdarg.com>
2025-01-03 22:18:32 -0300

    add helper

--- src/utils/logic/orders.js
+++ src/utils/logic/orders.js
@@ -112,9 +112,15 @@ export const formatCreatedAt = (createdAt) => {
 		day: '2-digit',
 		month: '2-digit',
 		year: 'numeric',
-	 /*  hour: '2-digit', */
+		/*  hour: '2-digit', */
 		/* minute: '2-digit', */
 		/* second: '2-digit', */
 	});
 	return formattedDate;
+};
+
+export const joinItems = (item) => {
+	const joinedItems = item.join(', ');
+	console.log(joinedItems);
+	return joinedItems;
 };

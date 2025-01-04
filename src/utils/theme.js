072836fda6e457b059378bc450073c15d6bb61aa
yamiltauil <ytauil@itcrowdarg.com>
2025-01-03 22:56:09 -0300

    update color schema

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,26 +1,41 @@
 export const darkTheme = {
-  bg: '#181818',
-  bgLighter: '#202020',
-  text: 'white',
-  textSoft: '#aaaaaa',
-  soft: '#373737',
-  hover: '#f5f5f5',
-  transparent: 'rgba(0, 0, 0, 0.6)',
-  orange: '#B28E70',
-  lightGreen: '#556B2F',
-  darkBlue: '#638B8E',
-  red: '#946262',
+	bg: '#181818',
+	bgLighter: '#202020',
+	text: 'white',
+	textSoft: '#aaaaaa',
+	soft: '#373737',
+	hover: '#f5f5f5',
+	transparent: 'rgba(0, 0, 0, 0.6)',
+	orange: '#B28E70',
+	lightGreen: '#556B2F',
+	darkBlue: '#638B8E',
+	red: '#946262',
 };
+/* export const lightTheme = { */
+/*   bg: '#f9f9f9', */
+/*   bgLighter: 'white', */
+/*   text: 'black', */
+/*   textSoft: '#606060', */
+/*   soft: '#aaaaaa', */
+/*   hover: '#373737', */
+/*   transparent: 'rgba(0, 0, 0, 0.6)', */
+/*   orange: '#FFA500', */
+/*   lightGreen: '#90EE90', */
+/*   darkBlue: '#ADD8E6', */
+/*   red: '#FFB6C1', */
+/* }; */
+/*  */
 export const lightTheme = {
-  bg: '#f9f9f9',
-  bgLighter: 'white',
-  text: 'black',
-  textSoft: '#606060',
-  soft: '#aaaaaa',
-  hover: '#373737',
-  transparent: 'rgba(0, 0, 0, 0.6)',
-  orange: '#FFA500',
-  lightGreen: '#90EE90',
-  darkBlue: '#ADD8E6',
-  red: '#FFB6C1',
+	bg: '#fdf1f1',
+	bgLighter: '#fbe3e3',
+	text: '#131313',
+	textSoft: '##646464',
+	soft: '#aaaaaa',
+	hover: '#373737',
+	transparent: 'rgba(0, 0, 0, 0.6)',
+	//orders colors
+	orange: '#FFA500',
+	lightGreen: '#90EE90',
+	darkBlue: '#ADD8E6',
+	red: '#FFB6C1',
 };
